67e5e6cab434b4fc5040f2b0aadeb394e946a9a1
manveti <manveti@users.noreply.github.com>
2014-05-26 22:20:57 -0700

    Tie in to Parts module to update (and eventually write out) parts loaded from game data files

--- SEDataFiles.py
+++ SEDataFiles.py
@@ -1,6 +1,13 @@
 import os.path
 import xml.etree.ElementTree
 
+import Parts
+
+from Constants import *
+
+
+SIZE_MAP = {'Small': TYPE_SM, 'Large': TYPE_LG}
+
 
 #####
 ##
@@ -97,10 +104,10 @@ def getBlocks(fname, components=None):
 			print "Unable to determine block (%s) size" % blockId.text
 			continue
 		valid = True
-		block['size'] = []
+		block[Parts.SIZE] = []
 		for dim in ['x', 'y', 'z']:
 			if (dim in blockSize.attrib):
-				block['size'].append(blockSize.attrib[dim])
+				block[Parts.SIZE].append(blockSize.attrib[dim])
 			else:
 				#warn: unable to determine block size
 				valid = False
@@ -112,7 +119,7 @@ def getBlocks(fname, components=None):
 		if (blockComponents is None):
 			#warn: unable to determine block components
 			continue
-		block['mass'] = 0
+		block[Parts.MASS] = 0
 		for comp in blockComponents.findall('Component'):
 			compId = comp.attrib.get('Subtype')
 			if (compId is None):
@@ -129,16 +136,16 @@ def getBlocks(fname, components=None):
 				#warn: unable to determine component count
 				valid = False
 				break
-			block['mass'] += components[compId] * compCount
+			block[Parts.MASS] += components[compId] * compCount
 		if (not valid):
 			continue
 
-		block['power'] = 0
+		block[Parts.POWER] = 0
 		for key in ['RequiredPowerInput', 'OperationalPowerConsumption', 'MaxPowerConsumption']:
 			powerNode = definition.find(key)
 			if ((powerNode is not None) and (powerNode.text)):
 				try:
-					block['power'] = float(powerNode.text) * 1000
+					block[Parts.POWER] = float(powerNode.text) * 1000
 				except ValueError:
 					#warn: unable to determine power consumption
 					valid = False
@@ -148,7 +155,7 @@ def getBlocks(fname, components=None):
 		powerNode = definition.find('MaxPowerOutput')
 		if ((powerNode is not None) and (powerNode.text)):
 			try:
-				block['power'] = -float(powerNode.text) * 1000
+				block[Parts.POWER] = -float(powerNode.text) * 1000
 			except ValueError:
 				#warn: unable to determine power generation
 				continue
@@ -167,12 +174,45 @@ def getBlocks(fname, components=None):
 #####
 				continue
 			if (blockClass.text == "Thrust"):
-				block['thrust'] = blockForce
+				block[Parts.THRUST] = blockForce
 			elif (blockClass.text == "Gyro"):
-				block['turn'] = blockForce
+				block[Parts.TURN] = blockForce
 
 		if (blockType.text not in retval):
 			retval[blockType.text] = {}
 		retval[blockType.text][blockId.text] = block
 
 	return retval
+
+def generateParts(blockDir, partsDir):
+	blocks = getBlocks(os.path.join(blockDir, "CubeBlocks.sbc"), os.path.join(blockDir, "Components.sbc"))
+	if (not blocks):
+		return
+	Parts.init()
+	for size in blocks.keys():
+		if (size not in SIZE_MAP):
+#####
+##
+			print "Skipping invalid block size %s" % size
+			continue
+##
+#####
+		for blockName in blocks[size].keys():
+			if (SIZE_MAP[size] not in Parts.parts):
+				Parts.parts[SIZE_MAP[size]] = {}
+			part = Parts.parts[SIZE_MAP[size]].get(blockName, Parts.Part(blockName, {}))
+			for i in xrange(3):
+				part.size[i] = blocks[size][blockName][Parts.SIZE][i]
+			part.mass = blocks[size][blockName][Parts.MASS]
+			part.power = blocks[size][blockName][Parts.POWER]
+			if (Parts.THRUST in blocks[size][blockName]):
+				part.thrust = blocks[size][blockName][Parts.THRUST]
+			if (Parts.TURN in blocks[size][blockName]):
+				part.turn = blocks[size][blockName][Parts.TURN]
+			Parts.parts[SIZE_MAP[size]][blockName] = part
+
+#####
+##
+	#Parts.writeParts(partsDir)
+##
+#####
